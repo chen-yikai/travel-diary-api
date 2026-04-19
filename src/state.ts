fe0c5e4c1f97d9559d32446eb91d219e69fee9d6
elias <eliaschen.dev@gmail.com>
2026-04-20 07:09:48 +0800

    clean up

--- src/state.ts
+++
@@ -1,9 +0,0 @@
-export type FavoriteDiaryData = {
-  diary_id: string;
-  favorite_datetime: string;
-};
-
-// State
-export const users = new Map<string, string>(); // email -> authToken
-export const passwords = new Map<string, string>(); // email -> password
-export const favorites: FavoriteDiaryData[] = [];
